554dfdb678d3040b3542ee6c4d14295eea59fa08
baiyao105 <nell_baiyao@outlook.com>
2025-07-03 23:11:52 +0800

    chore: 去除一些多余的东西

--- Sundry/rewritedb.sh
+++ Sundry/rewritedb.sh
@@ -2,20 +2,6 @@
 # Script by XTC-ThemePro - @baiyao105
 MODPATH=$(cd "$(dirname "$0")/.." && pwd)
 theme_db="/data/user/0/com.xtc.theme/databases"
-am start com.xtc.theme/.view.SplashActivity
 am force-stop com.xtc.theme
-mkdir -p "${theme_db}" || true
-chmod 755 "${theme_db}"/* || true
-rm -rf "${theme_db}/theme_package.db" || true
 cp -af "${MODPATH}/theme_package.db" "${theme_db}/theme_package.db" || true
 cp -af "${MODPATH}/personality_charge.db" "${theme_db}/personality_charge.db" || true
-sleep 3
-cp -af "${MODPATH}/theme_package.db" "${theme_db}/theme_package.db" || true
-cp -af "${MODPATH}/personality_charge.db" "${theme_db}/personality_charge.db" || true
-sleep 5
-cp -af "${MODPATH}/theme_package.db" "${theme_db}/theme_package.db" || true
-cp -af "${MODPATH}/personality_charge.db" "${theme_db}/personality_charge.db" || true
-touch /data/user/0/com.xtc.theme/databases/theme_package.db-journal
-touch /data/user/0/com.xtc.theme/databases/personality_charge.db-journal
-chmod 700 "/data/user/0/com.xtc.theme/databases/theme_package.db" || true
-chmod 700 "/data/user/0/com.xtc.theme/databases/personality_charge.db" || true
