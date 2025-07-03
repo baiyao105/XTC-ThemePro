58b8738eeb976b6fe83e9c459703c47513b3c50d
baiyao105 <nell_baiyao@outlook.com>
2025-07-03 20:38:10 +0800

    fix: 一言获取不随机

--- files/themepro.sh
+++ files/themepro.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:abdd35fc51f15c669edeec0e48daa6afbedfeee55589dc00b63aeb356e9e7d20
-size 3632
+oid sha256:8a3e5ceef0fc13694b3e64aa19609e1886249c3775b668ac3c814530a281a184
+size 3902
