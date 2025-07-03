7cb83e8d68ca7981199c0fe415188d2f62f2201d
白杳 <nell_baiyao@outlook.com>
2025-07-03 14:40:19 +0800

    fix: 安卓grep参数使用错误

--- files/themepro.sh
+++ files/themepro.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:314002c2b23ffcca4beb4da1c0d0f7520e6c48ae64f04cec59de167da87f2869
-size 3274
+oid sha256:350185b119b6cf06b38cf524ead06351ba7b039e4e50949f4b61ba896e8fe2be
+size 3618
