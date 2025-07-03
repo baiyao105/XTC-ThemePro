4afe900075fc37d89f6f49e33f729a7f0dde663f
baiyao105 <nell_baiyao@outlook.com>
2025-07-03 15:05:58 +0800

    fix: 动态加载的属性(cta...)无法被正确读取

---
+++ files/post-fs-data.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:075d6da4c1a71fc20ed04c16f5d0c43bc5aed0b88120ce3b494911548cebd745
+size 680
--- files/themepro.sh
+++ files/themepro.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:350185b119b6cf06b38cf524ead06351ba7b039e4e50949f4b61ba896e8fe2be
-size 3618
+oid sha256:abdd35fc51f15c669edeec0e48daa6afbedfeee55589dc00b63aeb356e9e7d20
+size 3632
