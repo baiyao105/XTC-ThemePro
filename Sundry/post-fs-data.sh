e3684c34c1e7d0d27d4966ac36e452d84269961d
baiyao105 <nell_baiyao@outlook.com>
2025-07-03 23:04:37 +0800

    chore: 优化安装流程

--- Sundry/post-fs-data.sh
+++ Sundry/post-fs-data.sh
@@ -1,15 +1,30 @@
 #!/sbin/sh
 # Script by XTC-ThemePro - @baiyao105
 MODDIR=${0%/*}
-cp "$MODDIR/theme_package.db" /data/user/0/com.xtc.theme/databases/theme_package.db
-cp "$MODDIR/personality_charge.db" /data/user/0/com.xtc.theme/databases/personality_charge.db
+cp -af "$MODDIR/theme_package.db" "/data/user/0/com.xtc.theme/databases/theme_package.db"
+cp -af "$MODDIR/personality_charge.db" "/data/user/0/com.xtc.theme/databases/personality_charge.db"
+chmod 700 "/data/user/0/com.xtc.theme/databases/theme_package.db" || true
+chmod 700 "/data/user/0/com.xtc.theme/databases/personality_charge.db" || true
 while [ "$(getprop sys.boot_completed)" != "1" ]; do
     sleep 5
 done
-best_text=$(themepro get.hitokoto)
+if [ "$(themepro getinstall)" != "true" ]; then
+    am force-stop com.xtc.theme
+    pm clear com.xtc.theme
+    sleep 7
+    themepro getinstall true
+    cp -af "$MODDIR/theme_package.db" "/data/user/0/com.xtc.theme/databases/theme_package.db"
+    cp -af "$MODDIR/personality_charge.db" "/data/user/0/com.xtc.theme/databases/personality_charge.db"
+    touch /data/user/0/com.xtc.theme/databases/theme_package.db-journal
+    touch /data/user/0/com.xtc.theme/databases/personality_charge.db-journal
+    chmod 700 "/data/user/0/com.xtc.theme/databases/theme_package.db" || true
+    chmod 700 "/data/user/0/com.xtc.theme/databases/personality_charge.db" || true
+    pm clear com.xtc.theme
+fi
+best_text=$(themepro gethitokoto)
 chmod +x "/system/bin/themepro"
 chmod +x "${MODDIR}/themepro.sh"
 chmod +x "${MODDIR}/Sundry/rewritedb.sh"
 sh "${MODDIR}/Sundry/rewritedb.sh"
 sed -i '/^description=/d' "${MODDIR}/module.prop"
-echo "description=${best_text} 🌸 为主题加点新花样 - 上次crontab时间: 等待运行" >>"${MODDIR}/module.prop"
+echo "description=🌸 为主题加点新花样 - ●${best_text} - 上次crontab时间: 等待运行" >> "${MODDIR}/module.prop"
--- files/themepro.sh
+++ files/themepro.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ab27406281b96c7a910418e3b2e6dbc153d6eec8076dc33bdee02f0d2179a30
-size 3922
+oid sha256:f84a172cc55475524ede383f59b7df98de0db326176ee4cf836e2974ec56f342
+size 4146
