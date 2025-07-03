3fcd2e984bab11128683cabfa9b7a3d23bfd29b6
baiyao105 <nell_baiyao@outlook.com>
2025-07-03 20:55:17 +0800

    fix: 一言获得格式错误

--- files/themepro.sh
+++ files/themepro.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a3e5ceef0fc13694b3e64aa19609e1886249c3775b668ac3c814530a281a184
-size 3902
+oid sha256:4ab27406281b96c7a910418e3b2e6dbc153d6eec8076dc33bdee02f0d2179a30
+size 3922
