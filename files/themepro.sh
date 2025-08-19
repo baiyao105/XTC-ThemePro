393a98dc982a2fb927be282b234942249d46f162
baiyao105 <nell_baiyao@outlook.com>
2025-08-19 17:37:20 +0800

    ci: 重构发布流程

--- files/themepro.sh
+++ files/themepro.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f84a172cc55475524ede383f59b7df98de0db326176ee4cf836e2974ec56f342
-size 4146
+oid sha256:865bdfc82f319f0ddbd9ec262313a610d625a193f97bfe3b4c11dbd5267270ce
+size 4147
